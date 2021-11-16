7069b132e2548926a9e07eaca7d7ccff5b9bc92d
sushi.at <sushi@opensky.to>
2021-11-16 18:14:25 +0000

    Added 70% of flight log creation

--- OpenSky.FlightLogXML/FlightLog.cs
+++ OpenSky.FlightLogXML/FlightLog.cs
@@ -6,6 +6,8 @@
 
 namespace OpenSky.FlightLogXML
 {
+    using System.Diagnostics;
+    using System.Linq;
     using System.Xml.Linq;
 
     /// -------------------------------------------------------------------------------------------------
@@ -16,8 +18,15 @@ namespace OpenSky.FlightLogXML
     /// sushi.at, 16/11/2021.
     /// </remarks>
     /// -------------------------------------------------------------------------------------------------
-    public class FlightLog
+    public partial class FlightLog
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The flight log file version.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private const string FlightLogFileVersion = "1.0";
+
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
         /// Generates a flight log.
@@ -31,11 +40,79 @@ namespace OpenSky.FlightLogXML
         /// -------------------------------------------------------------------------------------------------
         public XElement GenerateFlightLog()
         {
+            Debug.WriteLine("Generating flight log XML file...");
             var log = new XElement("OpenSky.FlightLog");
+            log.Add(new XElement("LogVersion", FlightLogFileVersion));
 
-            // todo
+            // Add some basic info about this log
+            log.Add(new XElement("Agent", this.Agent));
+            log.Add(new XElement("AgentVersion", this.AgentVersion));
+            log.Add(new XElement("OpenSkyUser", this.OpenSkyUser));
+            log.Add(new XElement("LocalTimeZone", $"{this.LocalTimeZone}"));
+            log.Add(new XElement("TrackingStarted", $"{this.TrackingStarted:O}"));
+            log.Add(new XElement("TrackingStopped", $"{this.TrackingStopped:O}"));
+            log.Add(new XElement("WasAirborne", this.WasAirborne));
+            log.Add(new XElement("WarpTimeSaved", $"{this.TimeSavedBecauseOfSimRate:c}"));
+            log.Add(new XElement("TotalPaused", $"{this.TotalPaused:c}"));
+
+            // Add flight basics
+            var flightElement = new XElement("Flight");
+            log.Add(flightElement);
+            flightElement.Add(new XElement("ID", $"{this.FlightID}"));
+            flightElement.Add(new XElement("PlaneRegistry", $"{this.AircraftRegistry}"));
+            flightElement.Add(new XElement("UtcOffset", $"{this.UtcOffset:F1}"));
+
+            flightElement.Add(this.Origin.GetXMLElement("Origin"));
+            flightElement.Add(this.Destination.GetXMLElement("Destination"));
+            flightElement.Add(this.Alternate.GetXMLElement("Alternate"));
+
+            flightElement.Add(new XElement("FuelGallons", $"{this.FuelGallons:F2}"));
+            flightElement.Add(new XElement("Payload", this.Payload));
+            flightElement.Add(new XElement("PayloadPounds", $"{this.PayloadPounds:F2}"));
+
+            // Add flight events
+            var eventLog = new XElement("EventLog");
+            log.Add(eventLog);
+            foreach (var entry in this.TrackingEventLogEntries)
+            {
+                eventLog.Add(entry.GetXMLElement());
+            }
+
+            // Add tracking map markers
+            var trackingMapMarkers = new XElement("EventMapMarkers");
+            log.Add(trackingMapMarkers);
+            foreach (var markerXML in this.TrackingEventMarkers.Select(marker => marker.GetXMLElement()).Where(markerXML => markerXML != null))
+            {
+                trackingMapMarkers.Add(markerXML);
+            }
+
+            // Add flight position reports
+            var positionReports = new XElement("PositionReports");
+            log.Add(positionReports);
+            foreach (var position in this.PositionReports)
+            {
+                positionReports.Add(position.GetXMLElement());
+            }
+
+            // Add landing report
+            var landingReport = new XElement("LandingReport");
+            log.Add(landingReport);
+            foreach (var touchDown in this.TouchDowns)
+            {
+                landingReport.Add(touchDown);
+            }
+
+            // Add nav log waypoints
+            var navLogWaypoints = new XElement("NavLogWaypoints");
+            log.Add(navLogWaypoints);
+            foreach (var waypoint in this.NavLogWaypoints)
+            {
+                navLogWaypoints.Add(waypoint);
+            }
 
             return log;
         }
+
+
     }
 }
---
+++ OpenSky.FlightLogXML/TrackingEventLogEntry.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TrackingEventLogEntry.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.FlightLogXML
+{
+    using System;
+    using System.Drawing;
+    using System.Xml.Linq;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// OpenSky tracking event.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 16/11/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public class TrackingEventLogEntry
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the altitude.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public int Altitude { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the color of the event.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public Color EventColor { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the date/time of the event.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public DateTime EventTime { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the type of the event.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public FlightTrackingEventType EventType { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public double Latitude { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the log message for the flight events log.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string LogMessage { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public double Longitude { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the log entry XElement for the flight log XML file.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 16/11/2021.
+        /// </remarks>
+        /// <returns>
+        /// The log entry.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public XElement GetXMLElement()
+        {
+            var log = new XElement("LogEntry");
+            log.SetAttributeValue("Type", $"{(int)this.EventType}");
+            log.SetAttributeValue("Time", $"{this.EventTime:O}");
+            log.SetAttributeValue("Lat", $"{this.Latitude}");
+            log.SetAttributeValue("Lon", $"{this.Longitude}");
+            log.SetAttributeValue("Alt", $"{this.Altitude}");
+            log.SetAttributeValue("Color", $"{this.EventColor}");
+            log.SetAttributeValue("Message", $"{this.LogMessage}");
+            return log;
+        }
+    }
+}
